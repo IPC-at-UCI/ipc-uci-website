6320040ea82753b7507d6463094d7c319639cc25
Noah Kim <noahmk5002@gmail.com>
2024-09-19 23:54:05 -0700

    Create events template

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link"
 
+import Navigation from "@/components/Navigation"
+import Section from "@/components/Section"
+import Footer from "@/components/Footer"
+
 export default function Page() {
     return (
-        <div className="w-screen h-screen bg-ipc-blue flex flex-col items-center justify-center text-ipc-white text-2xl">
-            <div className="font-bold text-6xl mb-8">This page is under construction!</div>
-            <div className="mb-4">IPC is working hard behind the scenes to deliver this page. Check back for updates soon!</div>
-            <Link href="/" className="font-bold text-ipc-yellow hover:underline underline-offset-4">Return home</Link>
+        <div>
+            <Navigation />
+            <Section className="h-[1000px] bg-ipc-black flex justify-center items-center">
+                <div>
+                    <div className="text-7xl font-bold text-ipc-yellow">Discover Events</div>
+                </div>
+            </Section>
+            <Section className="min-h-[1000px] bg-ipc-blue flex justify-center">
+                <div>
+                    <div className="text-5xl font-bold text-ipc-white mt-[100px]">See all IPC events</div>
+                </div>
+            </Section>
+            <Footer />
         </div>
     )
 }
