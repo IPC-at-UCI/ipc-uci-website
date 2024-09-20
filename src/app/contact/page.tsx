355b44a6659c1f41770f6aa6eada2b602c5415ba
Noah Kim <noahmk5002@gmail.com>
2024-09-19 23:52:33 -0700

    Complete contact, compress images

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,59 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+
+import Navigation from "@/components/Navigation";
+import Section from "@/components/Section";
+import Footer from "@/components/Footer";
+import ContactIcon from "@/components/ContactIcon";
+
+import rightCircuit from "@/app/_images/right-circuit.png";
+import leftCircuit from "@/app/_images/left-circuit.png";
+
+import { EnvelopeClosedIcon, DiscordLogoIcon, InstagramLogoIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function Page() {
-    return (
-        <div className="w-screen h-screen bg-ipc-blue flex flex-col items-center justify-center text-ipc-white text-2xl">
-            <div className="font-bold text-6xl mb-8">This page is under construction!</div>
-            <div className="mb-4">IPC is working hard behind the scenes to deliver this page. Check back for updates soon!</div>
-            <Link href="/" className="font-bold text-ipc-yellow hover:underline underline-offset-4">Return home</Link>
+  return (
+    <div>
+      <Navigation />
+
+      <Image
+        src={rightCircuit}
+        alt="Circuit design"
+        className="absolute right-0 translate-y-[820px] max-[1500px]:invisible"
+      />
+      <Image
+        src={leftCircuit}
+        alt="Circuit design"
+        className="absolute translate-y-[100px] max-[1500px]:invisible"
+      />
+
+      <Section className="bg-ipc-blue h-[1000px] flex justify-center items-center">
+        <div>
+          <div className="text-ipc-white text-6xl font-bold text-center mb-8">
+            Have Questions?
+          </div>
+          <div className="text-ipc-white text-3xl font-light text-center mb-[50px]">
+            Send us an email or DM us on our socials!
+          </div>
+          <div className="flex justify-center mb-[40px]">
+            <Link
+              href="#"
+              className="flex items-center px-7 py-4 bg-ipc-white text-ipc-gray gap-3 rounded-full hover:bg-gray-200 duration-200 w-fit"
+            >
+              <EnvelopeClosedIcon className="w-7 h-7" />
+              <div className="text-2xl">ipc.ucirvine@gmail.com</div>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-3 justify-center">
+            <ContactIcon Icon={DiscordLogoIcon} className="w-10 h-10" />
+            <ContactIcon Icon={InstagramLogoIcon} className="w-10 h-10" />
+            <ContactIcon Icon={EnvelopeClosedIcon} className="w-10 h-10" />
+            <ContactIcon Icon={GitHubLogoIcon} className="w-10 h-10" />
+          </div>
         </div>
-    )
+      </Section>
+      <Footer />
+    </div>
+  );
 }
--- src/components/ContactIcon.tsx
+++ src/components/ContactIcon.tsx
@@ -1,7 +1,13 @@
-export default function ContactIcon({ Icon }: { Icon: any }) {
+export default function ContactIcon({
+  Icon,
+  className,
+}: {
+  Icon: any;
+  className?: string;
+}) {
   return (
     <button className="py-2 px-2 hover:bg-ipc-black rounded-xl ease-in-out duration-200">
-      <Icon className={`text-ipc-white w-5 h-5`} />
+      <Icon className={`text-ipc-white w-5 h-5 ${className}`} />
     </button>
   );
 }
