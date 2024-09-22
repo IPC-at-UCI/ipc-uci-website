5361c8a36c84798d5a8419f5f4d3f35297406ab4
Noah Kim <noahmk5002@gmail.com>
2024-09-22 15:25:16 -0700

    Update readme

--- src/app/_data/events.ts
+++ src/app/_data/events.ts
@@ -1,35 +1,39 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
-import img4 from "@/app/_images/img4.jpg";
+import img2 from "./event-images/img2.jpg";
+import img3 from "./event-images/img3.jpg";
+import img4 from "./event-images/img4.jpg";
 
-interface EventObject {
+export interface EventObject {
   title: string;
   buildingName: string;
   location: string;
   date: Date;
-  description: string;
+  description: string[];
   images?: StaticImport[];
 }
 
-const EVENT_DATA: EventObject[] = [
+export const EVENT_DATA: EventObject[] = [
   {
     title: "2025 IPC APEX Expo",
     buildingName: "Anaheim Convention Center",
     location: "Anaheim, CA",
     date: new Date("2025-04-06"),
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta scelerisque quam ac hendrerit. Nullam blandit dolor ante, ac venenatis dui elementum volutpat. Aenean malesuada justo vel magna finibus accumsan. Aenean tincidunt bibendum urna at vulputate. Aliquam commodo, lacus et imperdiet tristique, massa tortor gravida velit, eget volutpat nibh augue sed neque. Vivamus convallis urna ac magna tempor eleifend. Nunc ante massa, lacinia sed metus consequat, vehicula sodales est. Nullam maximus consequat varius.\nPellentesque posuere, felis dapibus ultricies eleifend, mauris sapien tristique dolor, quis vehicula elit eros id ipsum. Duis auctor nec neque ultrices accumsan. Vivamus commodo, purus vitae scelerisque suscipit, massa leo imperdiet massa, et ultricies lectus tortor sed felis. Proin a lacus vel lorem vestibulum gravida sollicitudin a erat. Curabitur euismod nibh et nibh faucibus egestas. Proin ac venenatis tellus. Etiam vestibulum suscipit est et posuere. Curabitur scelerisque sapien libero, vitae bibendum lacus porta posuere. In sed libero nisl. Nulla tristique sapien non est sagittis tempor. Etiam cursus pulvinar lorem, sit amet fermentum enim hendrerit in.",
-    images: [img4],
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta scelerisque quam ac hendrerit. Nullam blandit dolor ante, ac venenatis dui elementum volutpat. Aenean malesuada justo vel magna finibus accumsan. Aenean tincidunt bibendum urna at vulputate. Aliquam commodo, lacus et imperdiet tristique, massa tortor gravida velit, eget volutpat nibh augue sed neque. Vivamus convallis urna ac magna tempor eleifend. Nunc ante massa, lacinia sed metus consequat, vehicula sodales est. Nullam maximus consequat varius.",
+      "Pellentesque posuere, felis dapibus ultricies eleifend, mauris sapien tristique dolor, quis vehicula elit eros id ipsum. Duis auctor nec neque ultrices accumsan. Vivamus commodo, purus vitae scelerisque suscipit, massa leo imperdiet massa, et ultricies lectus tortor sed felis. Proin a lacus vel lorem vestibulum gravida sollicitudin a erat. Curabitur euismod nibh et nibh faucibus egestas. Proin ac venenatis tellus. Etiam vestibulum suscipit est et posuere. Curabitur scelerisque sapien libero, vitae bibendum lacus porta posuere. In sed libero nisl. Nulla tristique sapien non est sagittis tempor. Etiam cursus pulvinar lorem, sit amet fermentum enim hendrerit in.",
+    ],
+    images: [img2, img3, img4, img2, img3, img4],
   },
   {
     title: "2025 IPC APEX Expo",
     buildingName: "Anaheim Convention Center",
     location: "Anaheim, CA",
-    date: new Date("2025-04-06"),
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta scelerisque quam ac hendrerit. Nullam blandit dolor ante, ac venenatis dui elementum volutpat. Aenean malesuada justo vel magna finibus accumsan. Aenean tincidunt bibendum urna at vulputate. Aliquam commodo, lacus et imperdiet tristique, massa tortor gravida velit, eget volutpat nibh augue sed neque. Vivamus convallis urna ac magna tempor eleifend. Nunc ante massa, lacinia sed metus consequat, vehicula sodales est. Nullam maximus consequat varius.\nPellentesque posuere, felis dapibus ultricies eleifend, mauris sapien tristique dolor, quis vehicula elit eros id ipsum. Duis auctor nec neque ultrices accumsan. Vivamus commodo, purus vitae scelerisque suscipit, massa leo imperdiet massa, et ultricies lectus tortor sed felis. Proin a lacus vel lorem vestibulum gravida sollicitudin a erat. Curabitur euismod nibh et nibh faucibus egestas. Proin ac venenatis tellus. Etiam vestibulum suscipit est et posuere. Curabitur scelerisque sapien libero, vitae bibendum lacus porta posuere. In sed libero nisl. Nulla tristique sapien non est sagittis tempor. Etiam cursus pulvinar lorem, sit amet fermentum enim hendrerit in.",
+    date: new Date("2024-04-06"),
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta scelerisque quam ac hendrerit. Nullam blandit dolor ante, ac venenatis dui elementum volutpat. Aenean malesuada justo vel magna finibus accumsan. Aenean tincidunt bibendum urna at vulputate. Aliquam commodo, lacus et imperdiet tristique, massa tortor gravida velit, eget volutpat nibh augue sed neque. Vivamus convallis urna ac magna tempor eleifend. Nunc ante massa, lacinia sed metus consequat, vehicula sodales est. Nullam maximus consequat varius.",
+      "Pellentesque posuere, felis dapibus ultricies eleifend, mauris sapien tristique dolor, quis vehicula elit eros id ipsum. Duis auctor nec neque ultrices accumsan. Vivamus commodo, purus vitae scelerisque suscipit, massa leo imperdiet massa, et ultricies lectus tortor sed felis. Proin a lacus vel lorem vestibulum gravida sollicitudin a erat. Curabitur euismod nibh et nibh faucibus egestas. Proin ac venenatis tellus. Etiam vestibulum suscipit est et posuere. Curabitur scelerisque sapien libero, vitae bibendum lacus porta posuere. In sed libero nisl. Nulla tristique sapien non est sagittis tempor. Etiam cursus pulvinar lorem, sit amet fermentum enim hendrerit in.",
+    ],
     images: [img4],
   },
 ];
-
-export default EVENT_DATA;
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,24 +1,70 @@
-import Link from "next/link"
+import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
-import Navigation from "@/components/Navigation"
-import Section from "@/components/Section"
-import Footer from "@/components/Footer"
+import Navigation from "@/components/Navigation";
+import Section from "@/components/Section";
+import Footer from "@/components/Footer";
+
+import { EVENT_DATA, EventObject } from "@/app/_data/events";
 
 export default function Page() {
-    return (
+  const eventPassed = (date: Date) => {
+    const today = new Date();
+    return today > date;
+  };
+
+  return (
+    <div className="overflow-x-hidden">
+      <Navigation />
+      <Section className="h-[1000px] bg-ipc-black flex justify-center items-center">
         <div>
-            <Navigation />
-            <Section className="h-[1000px] bg-ipc-black flex justify-center items-center">
-                <div>
-                    <div className="text-7xl font-bold text-ipc-yellow">Discover Events</div>
-                </div>
-            </Section>
-            <Section className="min-h-[1000px] bg-ipc-blue flex justify-center">
-                <div>
-                    <div className="text-5xl font-bold text-ipc-white mt-[100px]">See all IPC events</div>
-                </div>
-            </Section>
-            <Footer />
+          <div className="text-7xl font-bold text-ipc-yellow">
+            Discover Events
+          </div>
         </div>
-    )
+      </Section>
+      <Section className="min-h-[1000px] bg-ipc-blue flex justify-center">
+        <div className="container mx-auto mb-[100px]">
+          <div className="text-5xl font-bold text-ipc-white mt-[100px] mb-[40px]">
+            See all IPC events
+          </div>
+          <div className="flex flex-col gap-4 text-xl">
+            {EVENT_DATA.map((event: EventObject, key: number) => (
+              <div className={`p-[35px] rounded-3xl ${eventPassed(event.date) ? "bg-zinc-400" : "bg-ipc-white"}`} key={key}>
+                <div>
+                  <h2 className="font-bold text-2xl">{event.title}</h2>
+                </div>
+                <div className="flex justify-between mb-2 text-gray-600">
+                  <p>
+                    {event.buildingName} | {event.location}
+                  </p>
+                  <p className="text-ipc-gray">
+                    {event.date.toLocaleDateString()}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-3 mb-7">
+                  {event.description.map((paragraph: string, key: number) => (
+                    <p className="leading-snug" key={key}>
+                      {paragraph}
+                    </p>
+                  ))}
+                </div>
+                <div className="flex gap-4 overflow-x-scroll">
+                  {event.images?.map((image: StaticImport, key: number) => (
+                    <Image
+                      src={image}
+                      alt="Event image"
+                      key={key}
+                      className="h-[300px] w-fit"
+                    />
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Section>
+      <Footer />
+    </div>
+  );
 }
