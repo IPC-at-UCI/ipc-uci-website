5a12a2a0fe9ea25e514db243ec2f0392d81f7804
Noah Kim <noahmk5002@gmail.com>
2024-09-24 18:02:18 -0700

    Add upcoming/past events toggle

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -10,11 +10,19 @@ import Footer from "@/components/Footer";
 
 import EventThumbnail from "@/components/EventThumbnail";
 
-
 import { EVENT_DATA, EventObject } from "@/app/_data/events";
 
-
 export default function Page() {
+  const [upcomingVisible, setUpcomingVisible] = useState<boolean>(true);
+  const [pastVisible, setPastVisible] = useState<boolean>(false);
+
+  const toggleUpcoming = () => {
+    setUpcomingVisible(!upcomingVisible);
+  };
+  const togglePast = () => {
+    setPastVisible(!pastVisible);
+  };
+
   const eventPassed = (date: Date) => {
     const today = new Date();
     return today > date;
@@ -29,73 +37,97 @@ export default function Page() {
             Discover Events
           </div>
           <div className="flex gap-[40px] mx-8">
-            <EventThumbnail eventData={EVENT_DATA[0]} href="#event-1"/>
-            <EventThumbnail eventData={EVENT_DATA[1]} href="#event-2"/>
-            <EventThumbnail eventData={EVENT_DATA[2]} href="#event-3"/>
+            <EventThumbnail eventData={EVENT_DATA[0]} href="#event-1" />
+            <EventThumbnail eventData={EVENT_DATA[1]} href="#event-2" />
+            <EventThumbnail eventData={EVENT_DATA[2]} href="#event-3" />
           </div>
         </div>
       </Section>
       <Section className="min-h-[1000px] bg-ipc-blue flex justify-center">
         <div className="container mx-auto mb-[100px]">
-          <div className="text-5xl font-bold text-ipc-white mt-[100px] mb-[40px]">
-            See all IPC events
+          <div className="flex justify-between items-end mb-[40px] gap-[80px]">
+            <h2 className="text-5xl font-bold text-ipc-white mt-[100px]">
+              See all IPC events
+            </h2>
+            <div className="flex gap-3">
+              <button
+                onClick={toggleUpcoming}
+                className={`text-ipc-white text-xl px-4 py-3 hover:bg-ipc-black duration-200 rounded-lg ${upcomingVisible && "bg-ipc-black"}`}
+              >
+                Upcoming Events
+              </button>
+              <button
+                onClick={togglePast}
+                className={`text-ipc-white text-xl px-4 py-3 hover:bg-ipc-black duration-200 rounded-lg ${pastVisible && "bg-ipc-black"}`}
+              >
+                Past Events
+              </button>
+            </div>
           </div>
+
           <div className="flex flex-col gap-4 text-xl">
             {EVENT_DATA.map((event: EventObject, key: number) => {
               const [expanded, setExpanded] = useState<boolean>(false);
 
-              return (
-                <div
-                  id={event.id}
-                  className={`p-[35px] rounded-3xl ${
-                    eventPassed(event.date) ? "bg-zinc-400" : "bg-ipc-white"
-                  }`}
-                  key={key}
-                >
-                  <div className="overflow-y-hidden h-fit" >
-                    <div>
-                      <h2 className="font-bold text-2xl">{event.title}</h2>
-                    </div>
-                    <div className="flex justify-between mb-2 text-gray-600">
-                      <p>
-                        {event.buildingName} | {event.location}
-                      </p>
-                      <p className="text-gray-600">
-                        {event.date.toLocaleDateString()}
-                      </p>
-                    </div>
-                    <div className="flex flex-col gap-3">
-                      {event.description.map(
-                        (paragraph: string, key: number) => (
-                          <p className="leading-snug text-2xl" key={key}>
-                            {paragraph}
-                          </p>
-                        )
+              if ((upcomingVisible && !eventPassed(event.date)) || (pastVisible && eventPassed(event.date))) {
+                return (
+                  <div
+                    id={event.id}
+                    className={`p-[35px] rounded-3xl ${
+                      eventPassed(event.date) ? "bg-zinc-400" : "bg-ipc-white"
+                    }`}
+                    key={key}
+                  >
+                    <div className="overflow-y-hidden h-fit">
+                      <div>
+                        <h2 className="font-bold text-2xl">{event.title}</h2>
+                      </div>
+                      <div className="flex justify-between mb-2 text-gray-600">
+                        <p>
+                          {event.buildingName} | {event.location}
+                        </p>
+                        <p className="text-gray-600">
+                          {event.date.toLocaleDateString()}
+                        </p>
+                      </div>
+                      <div className="flex flex-col gap-3">
+                        {event.description.map(
+                          (paragraph: string, key: number) => (
+                            <p className="leading-snug text-2xl" key={key}>
+                              {paragraph}
+                            </p>
+                          )
+                        )}
+                      </div>
+                      {expanded && (
+                        <div className="flex gap-4 overflow-x-scroll mt-7">
+                          {event.images?.map(
+                            (image: StaticImport, key: number) => (
+                              <Image
+                                src={image}
+                                alt="Event image"
+                                key={key}
+                                className="w-[300px] rounded-2xl"
+                              />
+                            )
+                          )}
+                        </div>
                       )}
                     </div>
-                    {expanded && <div className="flex gap-4 overflow-x-scroll mt-7">
-                      {event.images?.map((image: StaticImport, key: number) => (
-                        <Image
-                          src={image}
-                          alt="Event image"
-                          key={key}
-                          className="w-[300px] rounded-2xl"
-                        />
-                      ))}
-                    </div>}
-                  </div>
 
-                  <div className="flex justify-end">
-                    <button className="mt-5 text-blue-500 font-medium hover:text-blue-700 hover:underline duration-200"
-                      onClick={() => {
-                        setExpanded(!expanded);
-                      }}
-                    >
-                      {expanded ? "Collapse" : "Expand"}
-                    </button>
+                    <div className="flex justify-end">
+                      <button
+                        className="mt-5 text-blue-500 font-medium hover:text-blue-700 hover:underline duration-200"
+                        onClick={() => {
+                          setExpanded(!expanded);
+                        }}
+                      >
+                        {expanded ? "Collapse" : "Expand"}
+                      </button>
+                    </div>
                   </div>
-                </div>
-              );
+                );
+              }
             })}
           </div>
         </div>
